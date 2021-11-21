ebd75f50368a36443a582774836b6e12af8edb7a
Sil9 <sil9gosy@mail.ru>
2021-11-21 14:19:43 +0300

    React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React, {ComponentType} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect} from "react-redux";
@@ -13,6 +11,8 @@ import {initializeApp} from "./Redux/app-reducer";
 import {AppRootStoreType} from "./Redux/redux-store";
 import {Preloader} from "./components/common/Preloader";
 
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"))
 
 class App extends React.Component<mapStatePropsType & mapDispatchPropsType> {
 
@@ -27,8 +27,12 @@ class App extends React.Component<mapStatePropsType & mapDispatchPropsType> {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className={'app-wrapper-content'}>
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={() =>{
+                        return <React.Suspense fallback={<Preloader/>}><DialogsContainer/></React.Suspense>
+                    } }/>
+                    <Route path='/profile/:userId?' render={() => {
+                        return <React.Suspense fallback={<Preloader/>}><ProfileContainer/></React.Suspense>
+                    }}/>
                     <Route path='/users' render={() => <UsersContainer/>}/>
                     <Route path='/login' render={() => <Login/>}/>
                 </div>
